091b97afbd260cbd2a9e6d2139404e54bc2f5989
Muhammad Ahad <ahadjaved382@gmail.com>
2025-05-03 13:33:34 +0500

    Second Assignment

---
+++ assignment2.py
@@ -0,0 +1,15 @@
+print("if-else condition code:")
+a = 86
+b = 308
+
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b are equal")
+else:
+    print("a is greater than b")
+
+print("for loop code:")
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
